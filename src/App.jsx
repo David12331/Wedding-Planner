f5f218a80ac246b3fc2d00bf72359121139029f0
David12331 <dpantoja1@pride.hofstra.edu>
2023-12-20 10:57:05 -0500

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  
+  const [activeSection, setActiveSection] = useState('ourStory');
+
+  const handleSectionChange = (section) => {
+    setActiveSection(section);
+  };
 
   return (
     <>
-<h1>hello kkkkkk</h1>
+      <header>
+        <h1>Wedding Planner</h1>
+      </header>
+
+      <nav>
+        <ul>
+          <li
+            className={activeSection === 'ourStory' ? 'active' : ''}
+            onClick={() => handleSectionChange('ourStory')}
+          >
+            Our Story
+          </li>
+          <li
+            className={activeSection === 'location' ? 'active' : ''}
+            onClick={() => handleSectionChange('location')}
+          >
+            Location
+          </li>
+          <li
+            className={activeSection === 'weather' ? 'active' : ''}
+            onClick={() => handleSectionChange('weather')}
+          >
+            Weather
+          </li>
+        </ul>
+      </nav>
+
+      <main>
+        {/* Content for each section goes here */}
+        {activeSection === 'ourStory' && <p>Our love story...</p>}
+        {activeSection === 'location' && <p>Details about the location...</p>}
+        {activeSection === 'weather' && <p>Weather information...</p>}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
